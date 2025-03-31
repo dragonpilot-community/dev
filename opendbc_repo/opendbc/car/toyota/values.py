290e1be46a09442f402e5bbe2b2a1ce5d1e42246
Rick Lan <ricklan@gmail.com>
2025-03-31 13:37:45 +0800

    brand/toyota/tss1-sng 2025-08-18: init 2025-11-03: move to raylib ui

--- opendbc_repo/opendbc/car/toyota/values.py
+++ opendbc_repo/opendbc/car/toyota/values.py
@@ -80,6 +80,7 @@ class ToyotaFlags(IntFlag):
   # these cars are speculated to allow stop and go when the DSU is unplugged
   SNG_WITHOUT_DSU_DEPRECATED = 512
 
+  TSS1_SNG = 2 ** 13
 
 def dbc_dict(pt, radar):
   return {Bus.pt: pt, Bus.radar: radar}
