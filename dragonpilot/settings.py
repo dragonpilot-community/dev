74b25d7ae5304928c04286542e51a3103eaa99f8
Rick Lan <ricklan@gmail.com>
2025-12-01 17:24:30 +0800

    2025-12-01: use settings

--- dragonpilot/settings.py
+++ dragonpilot/settings.py
@@ -42,6 +42,20 @@ SETTINGS = [
     "title": "UI",
     "condition": "not MICI",
     "settings": [
+      {
+        "key": "dp_ui_lead",
+        "type": "text_spin_button_item",
+        "title": "Display Lead Stats",
+        "description": "Display the statistics of lead car and/or radar tracking points.<br>Lead: Lead stats only<br>Radar: Radar tracking point stats only<br>All: Lead and Radar stats<br>NOTE: Radar option only works on certain vehicle models.",
+        "default": 0,
+        "options": [
+          "Off",
+          "Lead",
+          "Radar",
+          "All"
+        ],
+        "condition": "not MICI",
+      },
 
     ],
   },
