87008cae91cec9667f15370b3fe5d6a6ba125324
Rick Lan <ricklan@gmail.com>
2025-12-01 17:20:30 +0800

    2025-12-01: use settings

--- dragonpilot/settings.py
+++ dragonpilot/settings.py
@@ -35,6 +35,12 @@ SETTINGS = [
     "title": "Longitudinal",
     "condition": "openpilotLongitudinalControl",
     "settings": [
+      {
+        "key": "dp_lon_dtsc",
+        "type": "toggle_item",
+        "title": "Dynamic Turn Speed Control (DTSC)",
+        "description": "DTSC automatically adjusts the vehicle's predicted speed based on upcoming road curvature and grip conditions.<br>Originally from the openpilot TACO branch."
+      },
 
     ],
   },
