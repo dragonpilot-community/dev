2dd9929a26c8387ff7ea20aaac83a30edd88b5ca
Rick Lan <ricklan@gmail.com>
2025-12-01 17:30:11 +0800

    2025-12-01: use settings

--- dragonpilot/settings.py
+++ dragonpilot/settings.py
@@ -48,6 +48,18 @@ SETTINGS = [
   {
     "title": "Device",
     "settings": [
+      {
+        "key": "dp_dev_delay_loggerd",
+        "type": "spin_button_item",
+        "title": "Delay Starting Loggerd for:",
+        "description": "Delays the startup of loggerd and its related processes when the device goes on-road.<br>This prevents the initial moments of a drive from being recorded, protecting location privacy at the start of a trip.",
+        "default": 0,
+        "min_val": 0,
+        "max_val": 300,
+        "step": 5,
+        "suffix": " secs",
+        "special_value_text": "Off"
+      },
 
     ],
   },
