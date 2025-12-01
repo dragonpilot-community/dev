44d219b5782c52e216d0c277633a28fb4c41810a
Rick Lan <ricklan@gmail.com>
2025-12-01 17:22:12 +0800

    2025-12-01: use settings

--- dragonpilot/settings.py
+++ dragonpilot/settings.py
@@ -48,6 +48,19 @@ SETTINGS = [
   {
     "title": "Device",
     "settings": [
+      {
+        "key": "dp_dev_audible_alert_mode",
+        "type": "text_spin_button_item",
+        "title": "Audible Alert",
+        "description": "Std.: Stock behaviour.<br>Warning: Only emits sound when there is a warning.<br>Off: Does not emit any sound at all.",
+        "default": 0,
+        "options": [
+          "Std.",
+          "Warning",
+          "Off"
+        ],
+        "condition": "not LITE"
+      },
 
     ],
   },
