3df2dc0e2bd40d08a58743200bd576eb3d079f67
Rick Lan <ricklan@gmail.com>
2025-12-18 15:41:23 +0800

    2025-12-18: multilang fix

--- dragonpilot/settings.py
+++ dragonpilot/settings.py
@@ -51,14 +51,14 @@ SETTINGS = [
       {
         "key": "dp_dev_auto_shutdown_in",
         "type": "spin_button_item",
-        "title": "Auto Shutdown After",
-        "description": "0 mins = Immediately",
+        "title": lambda: tr("Auto Shutdown After"),
+        "description": lambda: tr("0 min = Immediately"),
         "default": -5,
         "min_val": -5,
         "max_val": 300,
         "step": 5,
-        "suffix": " mins",
-        "special_value_text": "Off"
+        "suffix": lambda: tr("min"),
+        "special_value_text": lambda: tr("Off"),
       },
 
     ],
