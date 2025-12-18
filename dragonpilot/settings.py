3e0018f33af56131dbe0548231a5f0ef6868527e
Rick Lan <ricklan@gmail.com>
2025-12-18 15:53:51 +0800

    2025-12-18: multilang fix

--- dragonpilot/settings.py
+++ dragonpilot/settings.py
@@ -51,14 +51,14 @@ SETTINGS = [
       {
         "key": "dp_dev_delay_loggerd",
         "type": "spin_button_item",
-        "title": "Delay Starting Loggerd for:",
-        "description": "Delays the startup of loggerd and its related processes when the device goes on-road.<br>This prevents the initial moments of a drive from being recorded, protecting location privacy at the start of a trip.",
+        "title": lambda: tr("Delay Starting Loggerd for:"),
+        "description": lambda: tr("Delays the startup of loggerd and its related processes when the device goes on-road.<br>This prevents the initial moments of a drive from being recorded, protecting location privacy at the start of a trip."),
         "default": 0,
         "min_val": 0,
         "max_val": 300,
         "step": 5,
-        "suffix": " secs",
-        "special_value_text": "Off"
+        "suffix": lambda: tr("sec"),
+        "special_value_text": lambda: tr("Off"),
       },
 
     ],
