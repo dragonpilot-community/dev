85665a2debf00fdcef1a84e01e1d15e2d296333d
Rick Lan <ricklan@gmail.com>
2025-12-18 15:40:29 +0800

    2025-12-18: multilang fix

--- dragonpilot/settings.py
+++ dragonpilot/settings.py
@@ -51,15 +51,15 @@ SETTINGS = [
       {
         "key": "dp_dev_audible_alert_mode",
         "type": "text_spin_button_item",
-        "title": "Audible Alert",
-        "description": "Std.: Stock behaviour.<br>Warning: Only emits sound when there is a warning.<br>Off: Does not emit any sound at all.",
+        "title": lambda: tr("Audible Alert"),
+        "description": lambda: tr("Std.: Stock behaviour.<br>Warning: Only emits sound when there is a warning.<br>Off: Does not emit any sound at all."),
         "default": 0,
         "options": [
-          "Std.",
-          "Warning",
-          "Off"
+          lambda: tr("Std."),
+          lambda: tr("Warning"),
+          lambda: tr("Off"),
         ],
-        "condition": "not LITE"
+        "condition": "not LITE",
       },
 
     ],
