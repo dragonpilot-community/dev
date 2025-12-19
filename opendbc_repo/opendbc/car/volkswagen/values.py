20cb7388784da78cf78044074502e46f5a2edbc8
Rick Lan <ricklan@gmail.com>
2025-12-19 21:39:39 +0800

    2025-12-19: merge fix

--- opendbc_repo/opendbc/car/volkswagen/values.py
+++ opendbc_repo/opendbc/car/volkswagen/values.py
@@ -190,6 +190,7 @@ class VolkswagenFlags(IntFlag):
   PQ = 2
   MLB = 8
 
+  A0SnG = 2 ** 10
 
 @dataclass
 class VolkswagenMLBPlatformConfig(PlatformConfig):
@@ -200,7 +201,6 @@ class VolkswagenMLBPlatformConfig(PlatformConfig):
   def init(self):
     self.flags |= VolkswagenFlags.MLB
 
-  A0SnG = 2 ** 10
 
 @dataclass
 class VolkswagenMQBPlatformConfig(PlatformConfig):
