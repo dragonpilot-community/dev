fe299a618bf61238f98d0c926d96abf0085ab2b3
Rick Lan <ricklan@gmail.com>
2026-02-05 12:29:02 +0800

    2026-02-05: doubles the decel slew rate, credit to @herizon1054

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -180,7 +180,7 @@ class LongitudinalPlanner:
       self.output_should_stop = output_should_stop_e2e or output_should_stop_mpc
 
     for idx in range(2):
-      accel_clip[idx] = np.clip(accel_clip[idx], self.prev_accel_clip[idx] - 0.05, self.prev_accel_clip[idx] + 0.05)
+      accel_clip[idx] = np.clip(accel_clip[idx], self.prev_accel_clip[idx] - (0.10 if self.aem._active else 0.05), self.prev_accel_clip[idx] + 0.05)
     self.output_a_target = np.clip(output_a_target, accel_clip[0], accel_clip[1])
     self.prev_accel_clip = accel_clip
 
