b09c7b0156641d428c4dec13b9ac0f867a8f7776
Rick Lan <ricklan@gmail.com>
2026-03-16 17:26:38 +0800

    feat: Squash all core features into min

---
+++ dragonpilot/settings.py
@@ -0,0 +1,54 @@
+try:
+  from dragonpilot.system.ui.lib.multilang import tr
+except:
+  from openpilot.system.ui.lib.multilang import tr
+
+SETTINGS = [
+  {
+    "title": "Toyota / Lexus",
+    "condition": "brand == 'toyota'",
+    "settings": [
+
+    ],
+  },
+  {
+    "title": "VAG",
+    "condition": "brand == 'volkswagen'",
+    "settings": [
+
+    ],
+  },
+  {
+    "title": "Mazda",
+    "condition": "brand == 'mazda'",
+    "settings": [
+
+    ],
+  },
+  {
+    "title": "Lateral",
+    "settings": [
+
+    ],
+  },
+  {
+    "title": "Longitudinal",
+    "condition": "openpilotLongitudinalControl",
+    "settings": [
+
+    ],
+  },
+  {
+    "title": "UI",
+    "condition": "not MICI",
+    "settings": [
+
+    ],
+  },
+  {
+    "title": "Device",
+    "settings": [
+
+    ],
+  },
+]
