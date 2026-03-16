c29025e62173917d9cc9a9f97e74b31295e1cc37
Rick Lan <ricklan@gmail.com>
2026-03-16 19:23:17 +0800

    remove solverExecutionTime - removed in 0.11.0 base

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -69,7 +69,6 @@ class LongitudinalPlanner:
     self.v_desired_trajectory = np.zeros(CONTROL_N)
     self.a_desired_trajectory = np.zeros(CONTROL_N)
     self.j_desired_trajectory = np.zeros(CONTROL_N)
-    self.solverExecutionTime = 0.0
     self.dtsc = DTSC(aggressiveness=0.8)
 
   @staticmethod
